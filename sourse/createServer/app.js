e95ce1d6c3ef7097d33f0fe16d323185bd4c2277
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-05-27 21:32:17 +0600

    add Base routing in server folder

--- sourse/createServer/app.js
+++ sourse/createServer/app.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = 3000;
 
@@ -7,29 +9,72 @@ const server = http.createServer((req, res) => {
     console.log('Server requst');
     console.log(req.url, req.method);
 
-     res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Content-Type', 'text/html');
+
+    const createPath = (page) => path.resolve(__dirname, "views", `${page}.html`);
+
+    let basePath = "";
+
+    switch(req.url) {
+        case '/':
+        case '/home':
+            basePath = createPath('index');
+            res.statusCode = 200;
+            break;
+            case '/about-us':
+            res.statusCode = 301;
+            res.setHeader('Location', '/contacts');
+            res.end();
+            break;
+        case '/about':
+            basePath = createPath('about');
+            break;
+        case '/contacts':
+            basePath = createPath('contacts');
+            res.statusCode = 200;
+            break;
+        default:
+            basePath = createPath('error');
+            res.statusCode = 404;
+            break;
+    }
+
+
+    fs.readFile(basePath, (err, data) => {
+        if (err) {
+            console.log(err)
+            res.statusCode = 500;
+            res.end();
+        }
+        else {
+            res.write(data);
+            res.end();
+        }
+    });
+});
+
 
     // res.write('<head><link rel="stylesheet" href="#"></link></head>');
 
     // res.write('<h1>Hello, world!</h1>');
     // res.write('<h2>Hello, world!</h2>');
 
-    const data = JSON.stringify([
-        {
-            id: 1,
-            text: 'Hello'
-        },
-        {
-            id: 2,
-            text: 'World'
-        },
-        {
-            id: 3,
-            text: '!'
-        }
-    ])
-    res.end(data);
-});
+    // const data = JSON.stringify([
+    //     {
+    //         id: 1,
+    //         text: 'Hello'
+    //     },
+    //     {
+    //         id: 2,
+    //         text: 'World'
+    //     },
+    //     {
+    //         id: 3,
+    //         text: '!'
+    //     }
+    // ])
+    // res.end(data);
+
 
 server.listen(PORT, 'localhost', (error) => {
     error ? console.log(error) : console.log(`Listening port ${PORT}`);
